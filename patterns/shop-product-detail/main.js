8f48416f2095f7463125af6f5d2ea95e9e910aa3
Oliver Hook <oliver.hook@porsche.digital>
2023-12-04 13:53:49 +0100

    added size chart

--- patterns/shop-product-detail/main.js
+++ patterns/shop-product-detail/main.js
@@ -1,1 +1,23 @@
 import './style.scss';
+
+/**
+ * Handle Added to Cart flyout
+ */
+const filterFlyout = document.getElementById('addedToCartFlyout');
+filterFlyout.addEventListener('dismiss', () => {
+  filterFlyout.open = false;
+});
+document.getElementById('addedToCartButton').addEventListener('click', () => {
+  filterFlyout.open = true;
+});
+
+/**
+ * Handle View Size Chart modal
+ */
+const sizeChartModal = document.getElementById('sizeChartModal');
+sizeChartModal.addEventListener('dismiss', () => {
+  sizeChartModal.open = false;
+});
+document.getElementById('sizeChartButton').addEventListener('click', () => {
+  sizeChartModal.open = true;
+});
