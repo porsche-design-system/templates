411321be0d87937af52d1b61eb3fc88da3c24063
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-12-12 20:34:50 +0100

    feat: add flyout search | sas

--- patterns/shop-product-list/main.js
+++ patterns/shop-product-list/main.js
@@ -10,6 +10,14 @@ flyoutMenu.addEventListener('dismiss', () => flyoutMenu.open = false);
 flyoutMenu.addEventListener('update', (e) => flyoutMenu.activeIdentifier = e.detail.activeIdentifier);
 
 /**
+ * Handle Flyout: Search
+ */
+const flyoutSearch = document.getElementById('flyout-search');
+const buttonSearch = document.getElementById('button-search');
+buttonSearch.addEventListener('click', () => flyoutSearch.open = true);
+flyoutSearch.addEventListener('dismiss', () => flyoutSearch.open = false);
+
+/**
  * Handle Flyout: Filter
  */
 const flyoutFilter = document.getElementById('flyout-filter');
