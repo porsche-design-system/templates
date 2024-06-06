b7dd8494839546a797d4be621d00dcef4281133b
Stephan Schröter <stephan.schroeter@me.com>
2024-06-06 14:25:54 +0200

    feat: improve grid productive poc pattern | sas

--- patterns/grid-productive/main.js
+++ patterns/grid-productive/main.js
@@ -1,1 +1,13 @@
 import './style.scss';
+
+document.querySelectorAll('button.toggle-sidebar-left').forEach((button) => {
+  button.addEventListener('click', () => {
+    document.querySelector('#app > aside:first-of-type').toggleAttribute('data-open');
+  })
+});
+
+document.querySelectorAll('button.toggle-sidebar-right').forEach((button) => {
+  button.addEventListener('click', () => {
+    document.querySelector('#app > aside:last-of-type').toggleAttribute('data-open');
+  })
+});
