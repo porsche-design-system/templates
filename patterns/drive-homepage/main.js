7e08b4daff1c7f5ffca627179d422758ef7f0f4b
Henri Joss <henri.joss@mhp.com>
2024-10-16 15:43:53 +0200

    feat: add reserve form + assets

--- patterns/drive-homepage/main.js
+++ patterns/drive-homepage/main.js
@@ -1,3 +1,8 @@
 import '../global.scss';
 import './style.scss';
 
+const reserveModal = document.querySelector('#reserve-station-modal');
+document.querySelector('#station-input').addEventListener('click', () => {
+  reserveModal.open = true;
+})
+reserveModal.addEventListener('dismiss', () => (reserveModal.open = false));
