781bfbe0cd499c41afc3e7cf68069be0649f1c01
Henri Joss <henri.joss@mhp.com>
2024-10-17 15:03:18 +0200

    feat: make reserve form sticky & add result hover

--- patterns/drive-homepage/main.js
+++ patterns/drive-homepage/main.js
@@ -6,3 +6,17 @@ document.querySelector('#station-input').addEventListener('click', () => {
   reserveModal.open = true;
 })
 reserveModal.addEventListener('dismiss', () => (reserveModal.open = false));
+
+const reserveDropdown = document.querySelector('.dropdown');
+const reserveResults = reserveDropdown.querySelectorAll('p-button-pure');
+const reserveImageContainer = reserveDropdown.querySelector('.dropdown__img');
+const reserveImg = reserveImageContainer.querySelector('img');
+const [reserveText, reserveAddress] = reserveImageContainer.querySelectorAll('p-text');
+
+reserveResults.forEach(result => {
+  result.addEventListener('mouseover', (e) => {
+    reserveImg.src = e.target.dataset.img;
+    reserveText.innerHTML = e.target.textContent;
+    reserveAddress.innerHTML = e.target.dataset.address;
+  })
+})
