393c69ad3989b87638547b6f90a3735af8bb9593
Henri Joss <henri.joss@mhp.com>
2024-10-21 15:08:03 +0200

    feat: add drive voucher page

--- patterns/drive-voucher/main.js
+++ patterns/drive-voucher/main.js
@@ -1,2 +1,25 @@
 import '../global.scss';
 import './style.scss';
+
+// Example code for demonstration purposes only. Do not use in production.
+
+const voucherCarSelect = document.querySelector('p-select[name="voucher-car"]');
+const voucherRateSelect = document.querySelector('p-select[name="voucher-rate"]');
+const voucherRate = voucherRateSelect.querySelectorAll('p-select-option');
+const voucherInput = document.querySelector('.input input');
+
+voucherCarSelect.addEventListener('update', (e) => {
+  const { value } = e.detail;
+  if (value) {
+    voucherRateSelect.disabled = false;
+    voucherRateSelect.value = voucherRate[1].value;
+    voucherInput.value = voucherRate[1].value;
+  } else {
+    voucherRateSelect.disabled = true;
+    voucherRateSelect.value = "";
+  }
+})
+
+voucherRateSelect.addEventListener('update', (e) => {
+  voucherInput.value = e.target.value;
+})
