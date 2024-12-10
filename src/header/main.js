e7f2db20bbb9e9926f856732bda47dbe759e0377
Stephan Schröter <stephan.schroeter@me.com>
2024-12-10 12:10:02 +0100

    feat: remove header template | sas

--- src/header/main.js
+++
@@ -1,8 +0,0 @@
-import './style.scss';
-
-const flyoutMenu = document.getElementById('flyout-menu');
-const buttonMenu = document.getElementById('button-menu');
-
-buttonMenu.addEventListener('click', () => flyoutMenu.open = true);
-flyoutMenu.addEventListener('dismiss', (e) => e.target.open = false);
-flyoutMenu.addEventListener('update', (e) => e.target.activeIdentifier = e.detail.activeIdentifier);
