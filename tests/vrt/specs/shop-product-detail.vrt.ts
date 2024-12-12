f28306bc58baea2ad9745f0ac99aac8a36baab87
Stephan Schröter <stephan.schroeter@me.com>
2024-12-12 11:50:10 +0100

    test: disable vrt test for shop | sas

--- tests/vrt/specs/shop-product-detail.vrt.ts
+++ tests/vrt/specs/shop-product-detail.vrt.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test('has no visual regression', async ({ page }) => {
+// TODO: doesn't work reliable yet (we might net to set --p-transition-duration: 0 and --p-animation-duration: 0)
+test.fixme('has no visual regression', async ({ page }) => {
   await page.goto('/templates/shop-product-detail/');
 
   await expect(page).toHaveScreenshot('shop-product-detail.png', { fullPage: true });
