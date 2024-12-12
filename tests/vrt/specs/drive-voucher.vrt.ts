78c40fe4c7c0b99b431cf05ad0f3d41b42f6a733
Stephan Schröter <stephan.schroeter@me.com>
2024-12-12 11:40:25 +0100

    test: disable vrt test for drive | sas

--- tests/vrt/specs/drive-voucher.vrt.ts
+++ tests/vrt/specs/drive-voucher.vrt.ts
@@ -1,6 +1,8 @@
 import { expect, test } from '@playwright/test';
 
-test('has no visual regression', async ({ page }) => {
+// TODO: doesn't work reliable yet (we might net to set --p-transition-duration: 0 and --p-animation-duration: 0)
+// TODO: we need to scroll down the page, to see all fade-in animated elements
+test.fixme('has no visual regression', async ({ page }) => {
   await page.goto('/templates/drive-voucher/');
 
   await expect(page).toHaveScreenshot('drive-voucher.png', { fullPage: true });
