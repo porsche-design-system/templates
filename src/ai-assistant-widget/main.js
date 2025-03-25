485fad7e148fdffd959f8553f2d465a7f1aa6fe6
Stephan Schröter <stephan.schroeter@me.com>
2025-03-25 11:47:57 +0100

    feat: improve ai widget template | sas

--- src/ai-assistant-widget/main.js
+++ src/ai-assistant-widget/main.js
@@ -1,17 +1,28 @@
 import './style.scss';
 
-// Example code for demonstration purposes only. Do not use in production.
-const modal = document.querySelector('p-modal');
-modal.addEventListener('dismiss', () => {
-  modal.open = false;
-});
+// DO NOT USE IN PRODUCTION!
+// EXAMPLE CODE FOR DEMONSTRATION PURPOSE ONLY.
 
 const updateTemplate = (id) => {
+  // remove template
   for (const template of document.querySelectorAll('[popover] > :not(template)')) {
     template.remove();
   }
+
+  // add template
   document.querySelector('[popover]').appendChild(document.querySelector(`template${id}`).content.cloneNode(true));
+
+  // simulate computing ai answers
+  setTimeout(() => {
+    for (const el of Array.from(document.querySelectorAll('.ai-answer[hidden],.ai-question[hidden]'))) {
+      el.removeAttribute('hidden');
+    }
+  }, 2000);
 };
 
+// update template based on hash url
 window.addEventListener('hashchange', () => updateTemplate(window.location.hash));
+
+// on init: update template and show widget
 updateTemplate(window.location.hash);
+document.querySelector('button.ai-button').click();
