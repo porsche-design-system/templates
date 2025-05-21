a435253b3ab1a75b07c5b9c17394c3e53f4d566a
Stephan Schröter <stephan.schroeter@me.com>
2025-05-21 11:52:48 +0200

    feat: migrate ai-assistant-widget to pds tailwindcss | sas

--- src/ai-assistant-widget/main.js
+++ src/ai-assistant-widget/main.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.css';
 
 // DO NOT USE IN PRODUCTION!
 // EXAMPLE CODE FOR DEMONSTRATION PURPOSE ONLY.
@@ -13,34 +13,34 @@ const updateTemplate = (id) => {
 };
 
 const removeTemplate = () => {
-  for (const template of document.querySelectorAll('.ai-popover > :not(template)')) {
+  for (const template of document.querySelectorAll('.c-popover > :not(template)')) {
     template.remove();
   }
 };
 
 const addTemplate = (id) => {
-  document.querySelector('.ai-popover').appendChild(document.querySelector(`template${id}`)?.content.cloneNode(true));
+  document.querySelector('.c-popover').appendChild(document.querySelector(`template${id}`)?.content.cloneNode(true));
 };
 
 const focusInput = () => {
-  document.querySelector('.ai-input')?.focus();
+  document.querySelector('.c-input')?.focus();
 };
 
 const focusButton = () => {
-  document.querySelector('.ai-button').focus();
+  document.querySelector('.c-button').focus();
 };
 
 const simulateAiChatRequest = () => {
   clearTimeout(t);
   t = setTimeout(() => {
-    for (const el of document.querySelectorAll('.ai-answer,.ai-question')) {
+    for (const el of document.querySelectorAll('.c-answer,.c-question')) {
       el.removeAttribute('hidden');
     }
   }, 2000);
 };
 
 const toggleAiWidget = () => {
-  document.querySelector('.ai-button').click();
+  document.querySelector('.c-button').click();
 };
 
 // update template based on hash url change
@@ -50,8 +50,8 @@ window.addEventListener('hashchange', () => updateTemplate(window.location.hash)
 toggleAiWidget();
 updateTemplate(window.location.hash);
 
-// close ai-popover on ESC
-document.querySelector('.ai-popover').onkeydown = (e) => {
+// close c-popover on ESC
+document.querySelector('.c-popover').onkeydown = (e) => {
   if (e.key === 'Escape') {
     toggleAiWidget();
     focusButton();
