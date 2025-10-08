0b063e994cb168dae9d162df6ae9961e014fa6f9
Stephan Schröter <stephan.schroeter@me.com>
2025-10-08 14:25:56 +0200

    feat(sdx26-finder): add sidebar | sas

--- src/sdx26-finder/main.js
+++ src/sdx26-finder/main.js
@@ -1,3 +1,7 @@
 import './style.css';
 // DO NOT USE IN PRODUCTION!
 // EXAMPLE CODE FOR DEMONSTRATION PURPOSE ONLY.
+
+for (const accordion of document.querySelectorAll('p-accordion')) {
+  accordion.addEventListener('update', (e) => (e.target.open = e.detail.open));
+}
