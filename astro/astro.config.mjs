fb7d1f5ec3920adb7b9e808d5d827d894d91ff9b
Stephan Schröter <stephan.schroeter@me.com>
2025-10-09 14:06:04 +0200

    feat(astro): integrate tailwindcss | sas

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // @ts-expect-error The types are currently mismatched due to conflicting Vite versions (Astro and the Tailwind CSS Vite plugin are using different versions of Vite)
+    plugins: [tailwindcss()],
+  },
+});
